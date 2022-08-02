5faf68520b336b51c8cfabe0b32e34f03ff6f339
kiran <kiran_1994@hotmail.com>
2022-08-02 20:28:14 +0100

    Flakey but working code

--- src/adaptive_controller/src/adaptive_controller.cpp
+++ src/adaptive_controller/src/adaptive_controller.cpp
@@ -5,13 +5,19 @@
 #include <fstream>
 #include <Eigen/Core>
 
-// % Initial Estimates
-#define m1 2
-#define m2 2
-#define l1 2
-#define l2 2
-#define Izz1 2
-#define Izz2 2
+// Initial Estimates
+#define m1 1
+#define m2 1
+#define l1 1
+#define l2 1
+#define Izz1 1
+#define Izz2 1
+
+// Custom Gains
+#define Kr 0.01
+#define Kv 0.01
+#define Kp 0.01
+#define Kgamma 1
 
 namespace adaptive_controller_ns
 {
@@ -20,7 +26,7 @@ namespace adaptive_controller_ns
         bool init(hardware_interface::EffortJointInterface* hw, ros::NodeHandle &n)
         {
             // Read from the csv file
-            std::ifstream file("/home/kiran/dissertation/ros_experimenting_ws/src/matlab_files/trajectory_test.csv");
+            std::ifstream file("/home/kiran/dissertation/ros_experimenting_ws/src/matlab_files/trajectory_test_100Hz.csv");
             trajectory = loadTrajectory(file);
             ROS_INFO("Trajectory points detected: %ld", trajectory.size());
 
@@ -42,7 +48,7 @@ namespace adaptive_controller_ns
             ROS_INFO("Initialisation complete!");
             prev_time = ros::Time::now();
 
-            // Eigen::MatrixXd theta_hat(5,1);
+            // Initialise theta_hat. TODO: See if you can move this to the constructor. It'll look neater.
             theta_hat(0,0) = m1*pow(l1,2) + m2*pow(l1,2) + m2*pow(l2,2) + Izz1 + Izz2;
             theta_hat(1,0) = m2*l1*l2;
             theta_hat(2,0) = m2*pow(l2,2) + Izz2;
@@ -58,46 +64,108 @@ namespace adaptive_controller_ns
         // TODO: Figure out exactly what these arguments mean
         void update(const ros::Time& time, const ros::Duration& period)
         {
+            // ros::Time curr_time = ros::Time::now();
+            // ros::Duration duration = time - prev_time;
+            // ROS_INFO("Elapsed sec: %f", duration.toSec());
+            // ROS_INFO("Elapsed sec: %f", duration.toSec());
 
-            
-            ros::Time curr_time = ros::Time::now();
-            ros::Duration duration = time - prev_time;
-            // ROS_INFO("Elapsed sec: %f", duration.toSec());
-            // ROS_INFO("Elapsed sec: %f", duration.toSec());
-            
-            // Compute errors
-            
-            
-            
-            prev_time = ros::Time::now();
+            // Make the robot state variables
+            Eigen::MatrixXd q_robot(2,1);
+            q_robot(0,0) = joints_[0].getPosition();
+            q_robot(1,0) = joints_[1].getPosition();
+
+            Eigen::MatrixXd qd_robot(2,1);
+            qd_robot(0,0) = joints_[0].getVelocity();
+            qd_robot(1,0) = joints_[1].getVelocity();
+
+            Eigen::MatrixXd qrd, qrdd, r_robot, theta_hat_d;
+
+            // Update theta_hat
+            if (traj_idx >= trajectory.size() - 1)
+            {
+                joints_[0].setCommand(0); // TODO: This should be set to the last trajectory command. 
+                joints_[1].setCommand(0);
+                return;
+            } else if (traj_idx > 0)
+            { 
+                computeError(traj_idx - 1, q_robot, qd_robot, qrd, qrdd, r_robot);
+                Eigen::MatrixXd Phi = getPhi(q_robot, qd_robot, qrd, qrdd); // Update Phi using prev trajectory and current robot states.     
+                Eigen::MatrixXd identity5x5 = Eigen:: MatrixXd::Identity(5, 5);
+                theta_hat_d = Kgamma*identity5x5*Phi.transpose()*r_robot; // TODO: Figure out how to get the transpose. Figure out how to get a diagonal matrix
+                theta_hat = theta_hat + theta_hat_d*period.toSec(); // NOTE: May have to do interpolation if periodicity is bad.
+            }
+
+            // Compute the control torque
+            computeError(traj_idx, q_robot, qd_robot, qrd, qrdd, r_robot);
+            Eigen::MatrixXd Phi = getPhi(q_robot, qd_robot, qrd, qrdd);
+            Eigen::MatrixXd tau = Phi*theta_hat + Kr*r_robot;
+
+            joints_[0].setCommand(tau(0,0));
+            joints_[1].setCommand(tau(1,0));
+
+            ROS_INFO("traj_idx: %d \n", traj_idx);
+            traj_idx++;
         }
 
         void starting(const ros::Time& time) { }
         void stopping(const ros::Time& time) { }
 
-        void getPhi()
+        Eigen::MatrixXd getPhi(Eigen::MatrixXd q_robot, Eigen::MatrixXd qd_robot, Eigen::MatrixXd qrd, Eigen::MatrixXd qrdd)
         {
-            // Make the state variables
-            Eigen::MatrixXd q_robot(2,1);
-            q_robot(0,0) = joints_[1].getPosition();
-            q_robot(1,0) = joints_[2].getPosition();
+            const double g_const = 9.81;
 
-            Eigen::MatrixXd qd_robot(2,1);
-            qd_robot(0,0) = joints_[1].getVelocity();
-            qd_robot(1,0) = joints_[2].getVelocity();
+            // TODO: Use the indexing instead of declaring more variables directly to save whitespace.
+            double q1 = q_robot(0,0);
+            double q2 = q_robot(1,0);
+            double q1d = qd_robot(0,0);
+            double q2d = qd_robot(1,0);
+            double qr1d = qrd(0,0);
+            double qr2d = qrd(1,0);
+            double qr1dd = qrdd(0,0);
+            double qr2dd = qrdd(1,0);
 
-            
             Eigen::MatrixXd Phi(2,5);
-            Phi(0,1) = 1;
-            Phi(0,2) = 1;
-            Phi(0,3) = 1;
-            Phi(0,4) = 1;
-            Phi(0,5) = 1;
-            Phi(1,1) = 1;
-            Phi(1,2) = 1;
-            Phi(1,3) = 1;
-            Phi(1,4) = 1;
-            Phi(1,5) = 1; 
+            Phi(0,0) = qr1dd;
+            Phi(0,1) = cos(q2)*(2*qr1dd + qr2dd) - sin(q2)*(q2d*qr1d + (q1d + q2d)*qr2d);
+            Phi(0,2) = qr2dd;
+            Phi(0,3) = g_const*cos(q1);
+            Phi(0,4) = g_const*cos(q1+q2);
+            Phi(1,0) = 0;
+            Phi(1,1) = qr1dd*cos(q2) + q1d*qr1d*sin(q2);
+            Phi(1,2) = qr1dd + qr2dd;
+            Phi(1,3) = 0;
+            Phi(1,4) = g_const*cos(q1 + q2);
+            return Phi;
+        }
+        
+        void computeError(unsigned int idx, Eigen::MatrixXd q_robot, Eigen::MatrixXd qd_robot, Eigen::MatrixXd& qrd, Eigen::MatrixXd& qrdd, Eigen::MatrixXd& r_robot)
+        {
+            Eigen::MatrixXd qt(2,1);
+            qt(0,0) = trajectory[idx][0];
+            qt(1,0) = trajectory[idx][1];
+           
+            Eigen::MatrixXd qtd(2,1);
+            qtd(0,0) = trajectory[idx][2];
+            qtd(1,0) = trajectory[idx][3];
+
+            Eigen::MatrixXd qtdd(2,1);
+            qtdd(0,0) = trajectory[idx][4];
+            qtdd(1,0) = trajectory[idx][5];
+
+            Eigen::MatrixXd e_robot = qt - q_robot;
+            Eigen::MatrixXd ed_robot = qtd - qd_robot;
+
+            qrd = qtd - Kp*e_robot;
+            qrdd = qtdd - Kv*ed_robot;
+            r_robot = qrd - qtd;
+
+            // std::cout << "e_robot: \n" << e_robot << std::endl;
+            // std::cout << "q_robot: \n" << q_robot << std::endl;
+            // std::cout << "qt: \n" << qt << std::endl;
+            // std::cout << "qrd: \n" << qrd << std::endl; //0.178867004605923,0.189288384507609
+            // std::cout << "qrdd: \n" << qrdd << std::endl;
+            // std::cout << "robot: \n" << r_robot << std::endl;
+
             return;
         }
         
@@ -126,7 +194,7 @@ namespace adaptive_controller_ns
 
         public:
             AdaptiveController() : theta_hat(5,1){ 
-                
+
             }
         
         private:
