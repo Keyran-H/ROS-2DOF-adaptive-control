27bd7b55914a34031af934022e218250f12d5ca4
kiran <kiran_1994@hotmail.com>
2022-08-11 15:57:43 +0100

    Found all the Phi matrices

--- src/matlab_files/novel_adaptive_controller_eqn_derivs.m
+++ src/matlab_files/novel_adaptive_controller_eqn_derivs.m
@@ -31,10 +31,10 @@ real_tau = M*qdd + Vm*qd + G;
 tau_sanity = isequaln(real_tau,sanity_tau)
 
 % syms th1 th2 th3 th4 th5
-Phi_vg = [0,(-2*q2d*q1d-q2d^2)*sin(q2),0,0,0;...
-          0,-q2d*q1d*sin(q2),0,0,0];
-% Phi_m2 = [0, q2d*q1d + (q1d + q2d)*q2d, 0, g*cos(q1), g*cos(q1 + q2);...
-%           0, -q1d^2, 0, 0, g*cos(q1 + q2)];
+% Phi_vg = [0,(-2*q2d*q1d-q2d^2)*sin(q2),0,0,0;...
+%           0,-q2d*q1d*sin(q2),0,0,0];
+Phi_vg = [0, -q1d*sin(q2)*q2d - q2d*sin(q2)*(q2d + q1d), 0, g*cos(q1), g*cos(q1 + q2); ...
+          0, sin(q2)*q1d^2, 0, 0, g*cos(q1 + q2)];
 Phi_m2 = [0, 2*q2d*sin(q2)*q1d + q2d^2*sin(q2), 0, 0, 0;...
           0,  q2d*sin(q2)*q1d, 0, 0, 0];
 Phi_m1 = [q1d, 2*cos(q2)*q1d + cos(q2)*q2d, q2d, 0, 0;...
@@ -48,7 +48,7 @@ f_sanity = isAlways(real_f==sanity_f)
 % Sanity check the Phi matrix for h = h1 + h2
 sanity_h = Phi_vg*theta_hat + Phi_m2*theta_hat;
 real_h = h1 + h2;
-% h_sanity = isAlways(real_h==sanity_h)
+h_sanity = isAlways(real_h==sanity_h)
 
 % Sanity check the Phi matrix for h1
 sanity_h1 = Phi_m2*theta_hat;
@@ -58,7 +58,7 @@ h1_sanity = isAlways(real_h1==sanity_h1)
 % Sanity check the Phi matrix for h2
 sanity_h2 = Phi_vg*theta_hat;
 real_h2 = h2;
-% h2_sanity = isAlways(real_h2==sanity_h2)
+h2_sanity = isAlways(real_h2==sanity_h2)
 
 
 % Md_test = [-2*theta_hat(2)*q2d*sin(q2), -theta_hat(2)*q2d*sin(q2);...
@@ -71,37 +71,19 @@ real_h2 = h2;
 % 
 % isAlways(M==M_test)
 
+% Vm_test = [-theta_hat(2)*sin(q2)*q2d, -theta_hat(2)*sin(q2)*(q1d + q2d);...
+%            theta_hat(2)*sin(q2)*q1d, 0]
+% 
+% isAlways(Vm==Vm_test)
 
-
+% G_test = [theta_hat(4)*g*cos(q1) + theta_hat(5)*g*cos(q1 + q2);...
+%           theta_hat(5)*g*cos(q1 + q2)]
+% 
+% isAlways(G==G_test)
 
 % q1d = cos(t/10 + 2)/10 + (16*cos(t/5 + 10))/5 + (27*cos((3*t)/10 + 12))/5
 % q2d = (8*cos(t/5 + 2))/5 + (9*cos((3*t)/10 + 10))/5 + (81*cos((9*t)/25 + 12))/25
 % 
 % q1dd = sin(t/10 + 2)/100 - (16*sin(t/5 + 10))/25 - (81*sin((3*t)/10 + 12))/50
 % q2dd = (8*sin(t/5 + 2))/25 - (27*sin((3*t)/10 + 10))/50 - (729*sin((9*t)/25 + 12))/625
-
-% Better sanity check
-% q1 = 3;
-% q2 = 5;
-% q1d = 7;
-% q2d = 11;
-% m1 = 13;
-% m2 = 17;
-% l1 = 19;
-% l2 = 23;
-% Izz1 = 4;
-% Izz2 = 7;
-% g = 9.81;
-
-% subs(real_f)
-% subs(sanity_f)
-
-% subs(real_h)
-% subs(sanity_h)
-
-% subs(real_h1)
-% subs(sanity_h1)
-
-% subs(real_h2)
-% subs(sanity_h2)
 
