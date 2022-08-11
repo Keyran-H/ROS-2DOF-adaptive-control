e43787a4becd61ec6f36003f3bdd5ab7e32e2015
kiran <kiran_1994@hotmail.com>
2022-08-11 20:58:09 +0100

    working checkpoint

---
+++ src/matlab_files/novel_adapter_sim.m
@@ -0,0 +1,157 @@
+clear
+close all
+
+% Define custom parameters
+simulation_time = 10; % seconds
+timeStep = 0.01; % seconds
+
+
+% Simulation calculations
+timeIntervals = 0:timeStep:simulation_time;
+
+% Robot State variables
+q_robot = [-0.3046; -0.0143]; % Make the robot start at joint position 0
+qd_robot = [0;0];
+
+% Custom Gains
+Kr = 1;
+Kv = 1;
+Kp = 0.1;
+gamma = 1*eye(5);
+
+% Initial Estimates
+m1 = 1;
+m2 = 1;
+l1 = 1;
+l2 = 1;
+Izz1 = 1;
+Izz2 = 1;
+theta_hat = [m1*l1^2 + m2*l1^2 + m2*l2^2 + Izz1 + Izz2; m2*l1*l2; m2*l2^2 + Izz2; m1*l1 + m2*l1; m2*l2]; % Double checked!
+
+theta_hat_data = [];
+e_robot_data = [];
+ed_robot_data = [];
+trajTimes = [];
+elapsed_time = 0;
+
+% Debug
+qdd_robot_debug = [];
+
+for i=timeIntervals
+
+    %%% Get the trajectory
+    [qt, qtd, qtdd] = GetTrajectoryPt(elapsed_time);
+
+    % Compute errors
+    e_robot = qt - q_robot;
+    qrd = qtd + Kp*e_robot;
+    ed_robot = qtd - qd_robot;
+    qrdd = qtdd + Kv*ed_robot;
+    r_robot = qrd - qd_robot;
+
+    % Compute control torques
+    Phi = GetPhi(q_robot, qd_robot, qrd, qrdd);
+    tau = Phi*theta_hat + Kr*r_robot;
+    
+    % Apply Torque to robot and update the robot state variables
+    [M, Vm, G] = getRobotDynamics(q_robot, qd_robot);
+    qdd_robot = -1*(M\(Vm*qd_robot + G - tau));
+    qd_robot = qd_robot + qdd_robot*timeStep;
+    q_robot = q_robot + qd_robot*timeStep;
+
+    % Recompute r and Phi (because the robot state changed)
+    e_robot = qt - q_robot;
+    qrd = qtd + Kp*e_robot;
+    ed_robot = qtd - qd_robot;
+    qrdd = qtdd + Kv*ed_robot;
+    r_robot = qrd - qd_robot;
+
+    Phi = GetPhi(q_robot, qd_robot, qrd, qrdd);
+
+    %%% Novel Adaptive Controller Adjustment %%%
+
+    % Get the filtered phi
+
+
+    %%%
+
+    % Update the adapted parameters
+    theta_hat_d = gamma*Phi'*r_robot;
+    theta_hat = theta_hat + theta_hat_d*timeStep;
+
+    % Save
+    theta_hat_data = [theta_hat_data theta_hat];
+    e_robot_data = [e_robot_data e_robot];
+    ed_robot_data = [ed_robot_data ed_robot];
+
+    qdd_robot_debug = [qdd_robot_debug qdd_robot];
+    elapsed_time = elapsed_time + timeStep;
+    trajTimes = [trajTimes elapsed_time];
+end
+
+figure
+title('Positin Error')
+plot(trajTimes,e_robot_data)
+xlabel('time')
+ylabel('position error')
+
+figure
+title('Velocity Errors')
+plot(trajTimes,ed_robot_data)
+
+xlabel('time')
+ylabel('velocity error')
+
+figure
+title('Parameter Convergence')
+plot(trajTimes,theta_hat_data)
+
+xlabel('time')
+
+function [qt, qtd, qtdd] = GetTrajectoryPt(t)
+    qt = [deg2rad(sin(0.1*t + 2) + 16*sin(0.2*t + 10) + 18*sin(0.3*t + 12));... % t=0 -> -17.4534
+          deg2rad(8*sin(0.2*t+2) + 6*sin(0.3*t+10) + 9*sin(0.36*t+12))];        % t=0 -> -0.8189
+    
+    qtd = [deg2rad(cos(t/10 + 2)/10 + (16*cos(t/5 + 10))/5 + (27*cos((3*t)/10 + 12))/5);...
+           deg2rad((8*cos(t/5 + 2))/5 + (9*cos((3*t)/10 + 10))/5 + (81*cos((9*t)/25 + 12))/25)];
+    
+    qtdd = [deg2rad(sin(t/10 + 2)/100 - (16*sin(t/5 + 10))/25 - (81*sin((3*t)/10 + 12))/50);
+            deg2rad((8*sin(t/5 + 2))/25 - (27*sin((3*t)/10 + 10))/50 - (729*sin((9*t)/25 + 12))/625)];    
+end
+
+function [M, Vm, G] = getRobotDynamics(q_robot, qd_robot)
+    m1 = 1;
+    m2 = 1;
+    l1 = 1;
+    l2 = 1;
+    Izz1 = 1;
+    Izz2 = 1;
+    g_const = 9.81;
+
+    M = [m1*l1^2 + m2*(l1^2+l2^2+2*l1*l2*cos(q_robot(2,1)) + Izz1 + Izz2), m2*(l2^2 + l1*l2*cos(q_robot(2,1))) + Izz2; ...
+         m2*(l2^2 + l1*l2*cos(q_robot(2,1))) + Izz2,                       m2*l2^2 + Izz2];
+    
+    h = -1*m2*l1*l2*sin(q_robot(2,1));
+    Vm = [h*qd_robot(2,1), h*(qd_robot(1,1) + qd_robot(2,1));...
+          -1*h*qd_robot(1,1), 0];
+
+    G = [g_const*(m1*l1 + m2*l1)*cos(q_robot(1,1)) + m2*l2*g_const*cos(q_robot(1,1) + q_robot(2,1));...
+         m2*l2*g_const*cos(q_robot(1,1) + q_robot(2,1))];
+end
+
+function Phi = GetPhi(q_robot, qd_robot, qrd, qrdd)
+    q1 = q_robot(1,1);
+    q2 = q_robot(2,1);
+    q1d = qd_robot(1,1);
+    q2d = qd_robot(2,1);
+    qr1d = qrd(1,1);
+    qr2d = qrd(2,1);
+    qr1dd = qrdd(1,1);
+    qr2dd = qrdd(2,1);
+    g_const = 9.81;
+
+    Phi = [qr1dd, cos(q2)*(2*qr1dd + qr2dd) - sin(q2)*(q2d*qr1d + (q1d + q2d)*qr2d), qr2dd, g_const*cos(q1), g_const*cos(q1+q2); ...
+           0, qr1dd*cos(q2) + q1d*qr1d*sin(q2), qr1dd + qr2dd, 0, g_const*cos(q1 + q2)];
+
+end
+
